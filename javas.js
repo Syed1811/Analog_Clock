575f278cc40e25c4f629f416e248dd7e0df179b8
Syed Raiyanullah <100852456+Syed1811@users.noreply.github.com>
2024-02-16 19:40:49 +0530

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,19 +1,17 @@
-// Just noticed accessing localStorage is banned from codepen, so disabling saving theme to localStorage
-
 const deg = 6;
 const hour = document.querySelector(".hour");
 const min = document.querySelector(".min");
 const sec = document.querySelector(".sec");
 
 const setClock = () => {
-	let day = new Date();
-	let hh = day.getHours() * 30;
-	let mm = day.getMinutes() * deg;
-	let ss = day.getSeconds() * deg;
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
 
-	hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-	min.style.transform = `rotateZ(${mm}deg)`;
-	sec.style.transform = `rotateZ(${ss}deg)`;
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
 };
 
 // first time
@@ -22,27 +20,24 @@ setClock();
 setInterval(setClock, 1000);
 
 const switchTheme = (evt) => {
-	const switchBtn = evt.target;
-	if (switchBtn.textContent.toLowerCase() === "light") {
-		switchBtn.textContent = "dark";
-		// localStorage.setItem("theme", "dark");
-		document.documentElement.setAttribute("data-theme", "light");
-	} else {
-		switchBtn.textContent = "light";
-		// localStorage.setItem("theme", "light"); //add this
-		document.documentElement.setAttribute("data-theme", "dark");
-	}
+    const switchBtn = evt.target;
+    if (switchBtn.textContent.toLowerCase() === "light") {
+        switchBtn.textContent = "dark";
+        // localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "light");
+    } else {
+        switchBtn.textContent = "light";
+        // localStorage.setItem("theme", "light"); //add this
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
 };
 
 const switchModeBtn = document.querySelector(".switch-btn");
 switchModeBtn.addEventListener("click", switchTheme, false);
 
-let currentTheme = "light";
-// currentTheme = localStorage.getItem("theme")
-// 	? localStorage.getItem("theme")
-// 	: null;
+let currentTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : "light"; // Set default theme to "light" if not stored in localStorage
 
-if (currentTheme) {
-	document.documentElement.setAttribute("data-theme", currentTheme);
-	switchModeBtn.textContent = currentTheme;
-}
+document.documentElement.setAttribute("data-theme", currentTheme);
+switchModeBtn.textContent = currentTheme;
